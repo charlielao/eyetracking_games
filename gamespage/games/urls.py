40fc4c0c1e92772a5b15e316d9694cd83701561d
charlielao <charliezz321@gmail.com>
2021-02-23 11:18:25 +0800

    ready to start adding game pages

--- gamespage/games/urls.py
+++ gamespage/games/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('frogger/', views.frogger, name='frogger'),
+    path('pacman/', views.pacman, name='pacman'),
 
 ]
--- gamespage/games/views.py
+++ gamespage/games/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def frogger(request):
+    return render(request, 'frogger.html')
+
+def pacman(request):
+    return render(request, 'pacman.html')
